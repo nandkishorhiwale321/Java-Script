d622fa2e4e7df31fa2d52914fc9bc7af54358ae8
nandkishorhiwale321 <nandkishorhiwale321@gmail.com>
2025-12-02 18:37:30 +0530

    nullish

---
+++ Function/Nullish.js
@@ -0,0 +1,18 @@
+
+/*Nullish Coalescing Operator(??)
+here null and undifined are falsy and other values (0,"") not change
+  ??
+*/
+let count = null;
+
+let total = count ?? 10;
+
+console.log(total);
+
+console.log("*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+const A = 0;
+
+const AA = A ?? "Hallo Jii"
+
+console.log(AA)
